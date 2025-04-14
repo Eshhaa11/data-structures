c87e7373d0df7e689a50fb87e31333d3a8af4e00
Esha Patel <Patellesha11@gmail.com>
2025-04-14 16:31:02 +0300

    added

---
+++ array.py
@@ -0,0 +1,15 @@
+#ARRAYS
+
+"""
+-> Arrays are fundamental data structures that store elements in a continous block of memory.
+-> Each element in an array , is identified by an index, starting from 0.
+-> Arrays provide constant time access to any elemtn using its index
+
+LEY CHARACTERISTICS
+->  Arrays can have a fixed size or be dynamic
+-> In strict arrays, all elements must be of the same type, in  pythons lists, they can store elements of different types (integers, strings, objects) i.e Heterogenous
+-> Elements are accessed using their index
+-> Elements are stored next to each other in memory (Contigous Memory), allowing fast access but potentially costly when resizing
+-> Lists can grow or shrink as needed by using methods like append() or pop()
+-> Lists can have references to objects, not raw data, so they use more memory than traditional arrays
+"""
